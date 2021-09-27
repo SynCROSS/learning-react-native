aeb5feb88f26f003336bcaf8c3fce066306d229a
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-09-27 13:29:45 +0900

    Create weather.list.ts

---
+++ lib/lists/weather.list.ts
@@ -0,0 +1,34 @@
+export const getKindOfWeathers = () => [
+  'Clouds',
+  'Clear',
+  'Rain',
+  'Mist',
+  'Dust',
+  'Thunderstorm',
+  'Haze',
+];
+
+export const getCloudsDescription = () => [
+  'broken clouds',
+  'overcast clouds',
+  'few clouds',
+  'scattered clouds',
+];
+
+export const getClearDescription = () => ['clear sky'];
+
+export const getRainDescription = () => [
+  'light rain',
+  'thunderstorm with rain',
+  'light intensity shower rain',
+  'moderate rain',
+  'shower rain',
+];
+
+export const getMistDescription = () => ['mist'];
+
+export const getDustDescription = () => ['dust'];
+
+export const getThunderStormDescription = () => ['thunderstorm with rain'];
+
+export const getHazeDescription = () => ['haze'];
