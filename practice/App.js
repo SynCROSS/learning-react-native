a9ccee5e15d7012a39ad82a9e1099439384d1c73
SynCROSS <kuuhaku3125@gmail.com>
2022-06-03 11:49:44 +0900

    Add Conditional Rendering

--- practice/App.js
+++ practice/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, SafeAreaView } from 'react-native';
 import Greeting from './components/Greeting';
 import Box from './components/Box';
 
 function App() {
+  const [visibility, setVisibility] = useState(true);
+
+  const onPress = useCallback(() => {
+    setVisibility(v => !v);
+  }, []);
+
   return (
     <SafeAreaView>
-      <Greeting />
-      <Box rounded />
+      <Button title={'Toggle visibility'} onPress={onPress} />
+      {visibility && (
+        <>
+          <Greeting />
+          <Box rounded size={'large'} backgroundColor={'#2FE58F'} />
+        </>
+      )}
     </SafeAreaView>
   );
 }
--- practice/components/Box.js
+++ practice/components/Box.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import { View } from 'react-native';
-import { StyleSheet } from 'react-native-windows';
+import { View, StyleSheet } from 'react-native';
 
-function Box({ rounded }) {
-  return <View style={[styles.box, rounded && styles.rounded]} />;
+const DEFAULT_SIZE = 'medium';
+
+const DEFAULT_BACKGROUND_COLOR = '#ff1740';
+function Box({
+  rounded,
+  size = DEFAULT_SIZE,
+  backgroundColor = DEFAULT_BACKGROUND_COLOR,
+}) {
+  return (
+    <View
+      style={[
+        styles.box,
+        rounded && styles.rounded,
+        sizes[size],
+        { backgroundColor },
+      ]}
+    />
+  );
 }
 
 const width = 64;
 const height = 64;
 
-const borderRadius = width / 4;
-
 const styles = StyleSheet.create({
   box: {
-    width,
-    height,
-    backgroundColor: '#ff1740',
+    backgroundColor: DEFAULT_BACKGROUND_COLOR,
   },
   rounded: {
-    borderRadius,
+    borderRadius: (width || height) / 4,
+  },
+  small: {
+    width: width / 2,
+    height: height / 2,
+  },
+  medium: {
+    width,
+    height,
+  },
+  large: {
+    width: width * 2,
+    height: height * 2,
   },
 });
+
+const sizes = {
+  small: styles?.small,
+  medium: styles?.medium,
+  large: styles?.large,
+};
+
+Box.defaultProps = {
+  size: DEFAULT_SIZE,
+  backgroundColor: DEFAULT_BACKGROUND_COLOR,
+};
 
 export default Box;
